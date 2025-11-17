96dd96bec6c261fd51ad87e3ab487fd62f257b27
ijfviana <ijfviana@gmail.com>
2025-11-17 13:51:26 +0100

    Add example environment configuration file for WildIntel tools

---
+++ env.example.py
@@ -0,0 +1,40 @@
+# wildintel-tools settings: {section}__{variable}
+
+# ==========================
+# [LOGGER]
+# ==========================
+# LOGGER__LOGLEVEL=1            # Log verbosity level (1=INFO, 2=DEBUG, etc.)
+# LOGGER__FILENAME=             # Optional log file path
+
+# ==========================
+# TRAPPER
+# ==========================
+# TAPPER__TRAPPER_USERNAME= "trapper_client@uhu.es"
+# TAPPER__TRAPPER_PASSWORD="pass"
+# TAPPER__TRAPPER_URL= "https://wildintel-trap.uhu.es/"
+# TAPPER__TRAPPER_TOKEN= ""
+
+# ==========================
+# ZOONIVERSE
+# ==========================
+
+#ZOONIVERSE__ZOONIVERSE_USERNAME=""
+#ZOONIVERSE__ZOONIVERSE_PASSWORD=""
+#ZOONIVERSE__ZOONIVERSE_PROJECT_ID=""
+
+# ==========================
+# ZOONIVERSE_CONNECTOR
+# ==========================
+#ZOONIVERSE_CONNECTOR__UPLOAD_COLLECTION_N_IMAGES_SEQ=5
+#ZOONIVERSE_CONNECTOR__UPLOAD_COLLECTION_MAX_INTERVAL=120
+#ZOONIVERSE_CONNECTOR__UPLOAD_COLLECTION_ATTEMPTS=5
+#ZOONIVERSE_CONNECTOR__UPLOAD_COLLECTION_DELAY=15
+#ZOONIVERSE_CONNECTOR__UPLOAD_COLLECTION_MAX_ATTEMPTS_PER_SUBJECT=5
+#ZOONIVERSE_CONNECTOR__UPLOAD_COLLECTION_DELAY_SECONDS_PER_SUBJECT=30
+
+# ==========================
+# docker environment variables
+# ==========================
+
+# Absolute path to your data directories (recommended)
+#DATA_PATH=./trapper-zooniverse-data/collections
